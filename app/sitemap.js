faf183b153209d058c4eb0fd2849d8d88138c065
tommygreer1 <tommygreer1@gmail.com>
2026-08-16 10:32:25 -0400

    Add Ellijay and North Georgia local SEO (#3)
    * Add Ellijay and North Georgia SEO metadata
    
    * Add XML sitemap for search engines
    
    * Add robots configuration for Google crawling
    
    * Optimize homepage copy for Ellijay local search

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.thequietalchemyreikiwellness.org",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
